ae5fa21ed56e7bf83013f516429aa840f48cc974
claudiofergarcia <clau.fernando.garcia@gmail.com>
2022-05-02 21:39:22 -0300

    Se agrega controlador y metodos Servicio

--- Ecommerce/src/main/java/com/ecommerce/entities/Producto.java
+++ Ecommerce/src/main/java/com/ecommerce/entities/Producto.java
@@ -18,6 +18,7 @@ public class Producto {
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+    private String nombre;
     private String descripcion;
     private int stock;
     private float precioVenta;
@@ -34,8 +35,9 @@ public class Producto {
     public Producto() {
     }
 
-    public Producto(String id, String descripcion, int stock, float precioVenta, TipoProducto tipoProducto, boolean activo, Date fechaAlta, Date fechaBaja) {
+    public Producto(String id, String nombre, String descripcion, int stock, float precioVenta, TipoProducto tipoProducto, boolean activo, Date fechaAlta, Date fechaBaja) {
         this.id = id;
+        this.nombre = nombre;
         this.descripcion = descripcion;
         this.stock = stock;
         this.precioVenta = precioVenta;
@@ -51,6 +53,14 @@ public class Producto {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getDescripcion() {
@@ -101,7 +111,7 @@ public class Producto {
         this.fechaAlta = fechaAlta;
     }
 
-    public Date getfechaBaja() {
+    public Date getFechaBaja() {
         return fechaBaja;
     }
 
@@ -109,4 +119,5 @@ public class Producto {
         this.fechaBaja = fechaBaja;
     }
 
+   
 }
