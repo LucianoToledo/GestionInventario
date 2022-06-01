745a62a21846e1359b88e586c818f56684481281
BelenCaseres <mbely.7@gmail.com>
2022-06-01 20:13:54 -0300

    controlador, producto

--- Ecommerce/src/main/java/com/ecommerce/repositories/ProductoRepositorio.java
+++ Ecommerce/src/main/java/com/ecommerce/repositories/ProductoRepositorio.java
@@ -2,6 +2,8 @@ package com.ecommerce.repositories;
 
 import com.ecommerce.entities.Producto;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +20,8 @@ public interface ProductoRepositorio extends JpaRepository<Producto, String> {
     
     @Query("SELECT p FROM Producto p WHERE p.nombre LIKE %:query%")
     public List<Producto> buscarPor(@Param("query") String query);
+    
+    @Query("SELECT p FROM Producto p WHERE p.nombre LIKE %:query%")
+    public Page<Producto> buscarPor(@Param("query") String query, Pageable pageable);
+    
 }
